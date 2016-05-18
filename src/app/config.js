bc1d102391e683991e1f5a195b4f353c6fac66e6
kwalker9 <klwalker9@gmail.com>
2016-05-18 17:10:03 -0600

    Release v1.0.0

--- src/app/config.js
+++ src/app/config.js
@@ -26,7 +26,7 @@ define([
 
         // version.: String
         //      The version number.
-        version: '0.1.1',
+        version: '1.0.0',
 
         // apiKey: String
         //      The api key used for services on api.mapserv.utah.gov
