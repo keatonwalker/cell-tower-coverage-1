bf908599f1dadc2635b374449f029d0860389eac
keatonwalker <keatwalker@gmail.com>
2016-05-25 15:42:31 -0600

    Release v1.1.0

--- src/app/config.js
+++ src/app/config.js
@@ -26,7 +26,7 @@ define([
 
         // version.: String
         //      The version number.
-        version: '1.0.0',
+        version: '1.1.0',
 
         // apiKey: String
         //      The api key used for services on api.mapserv.utah.gov
